c3a7bb067ced1b19abca2805702c670a14af63cb
Hai Liang Wang <hai@chatopera.com>
2025-05-29 11:37:30 +0800

    init

---
+++ scripts/publish.sh
@@ -0,0 +1,28 @@
+#! /bin/bash 
+###########################################
+#
+###########################################
+
+# constants
+baseDir=$(cd `dirname "$0"`;pwd)
+export PYTHONUNBUFFERED=1
+export PATH=/opt/miniconda3/envs/venv-py3/bin:$PATH
+
+# functions
+
+# main 
+[ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
+cd $baseDir/..
+
+if [ ! -d tmp ]; then
+    mkdir tmp
+fi
+
+if [ -f synonyms/data/words.vector.gz ]; then
+    echo "Move pkg to tmp"
+    mv synonyms/data/words.vector.gz tmp
+fi
+
+rm -rf ./dist/*
+python setup.py sdist
+twine upload --skip-existing dist/*
