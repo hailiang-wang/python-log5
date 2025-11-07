268dfb20466485dc9eea3bf7d54d78c103bb423c
Hai Liang Wang <hai@chatopera.com>
2025-11-07 16:07:04 +0800

    Enable log level as WARN

--- log5/__init__.py
+++ log5/__init__.py
@@ -66,6 +66,9 @@ LOG_FORMATTER = logging.Formatter(
 LOG_LEVEL= ENV.get("LOG_LEVEL", "INFO")
 LOG_FILE= ENV.get("LOG_FILE", None)
 
+if isinstance(LOG_LEVEL, str) and LOG_LEVEL.lower() == "warn":
+    LOG_LEVEL = "WARNING"
+
 print("[log5] logger settings LOG_FILE %s, LOG_LEVEL %s >> usage checkout https://github.com/hailiang-wang/python-log5" % (LOG_FILE, LOG_LEVEL))
 
 # log would print twice with logging.basicConfig
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ https://github.com/hailiang-wang/python-log5
 
 setup(
     name='log5',
-    version='0.0.9',
+    version='1.0.0',
     description='Output logs',
     long_description=LONGDOC,
     author='Hai Liang Wang',
